b5517544a12b6516007b455b2880a990cc9aedfc
Tazzmanian <tazzmanian@abv.bg>
2021-12-27 21:23:29 +0200

    Text controls

--- webapp/index.js
+++ webapp/index.js
@@ -1,16 +1,19 @@
 sap.ui.define([
 	"sap/m/Button",
-	"sap/m/MessageToast"
-], function (Button, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/m/Text"
+], function (Button, MessageToast, Text) {
 	"use strict";
 
-    alert('TEST')
+	// new Button({
+	// 	text: "Ready...",
+	// 	press: function () {
+	// 		MessageToast.show("Hello World!");
+	// 	}
+	// }).placeAt("content");
 
-	new Button({
-		text: "Ready...",
-		press: function () {
-			MessageToast.show("Hello World!");
-		}
-	}).placeAt("content");
+	new Text({
+		text: "Hello UI5!"
+	}).placeAt("content")
 
 });
