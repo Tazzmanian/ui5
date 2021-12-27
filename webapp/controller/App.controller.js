4daa679866ae7167e95648a7ef8d9b9f3e69801c
Tazzmanian <tazzmanian@abv.bg>
2021-12-27 22:39:31 +0200

    Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,14 +1,16 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ], function(
-	Controller
+	Controller,
+	MessageToast
 ) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.ui5.controller.App", {
         onShowHello : function () {
             // show a native JavaScript alert
-            alert("Hello World");
+            MessageToast.show('Hello there')
          }
 	});
 });
