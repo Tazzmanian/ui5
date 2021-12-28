ba9573a62921e8d84aabe4b374061c3de7e29ee4
Tazzmanian <tazzmanian@abv.bg>
2021-12-28 14:01:33 +0200

    Translatable text

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
     "sap/m/MessageToast", 
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
 ], function(
 	Controller,
 	MessageToast,
-	JSONModel
+	JSONModel,
+	ResourceModel
 ) {
 	"use strict";
 
@@ -17,12 +19,23 @@ sap.ui.define([
                   name : "UI5"
                }
             };
-            var oModel = new JSONModel(oData);
+            const oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
+            // set i18n model on view
+            const i18nModel = new ResourceModel({
+                bundleName: "sap.ui.demo.ui5.i18n.i18n",
+                supportedLocales: [""],
+                fallbackLocale: ""
+            });
+            this.getView().setModel(i18nModel, "i18n");
          },
         onShowHello : function () {
-            // show a native JavaScript alert
-            MessageToast.show('Hello there')
+            // read msg from i18n model
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            // show message
+            MessageToast.show(sMsg);
          }
 	});
 });
