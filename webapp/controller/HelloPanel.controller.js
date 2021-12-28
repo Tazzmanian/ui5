13467ca5834fe3cd31b1b293408e02df2aa57022
Tazzmanian <tazzmanian@abv.bg>
2021-12-28 15:54:06 +0200

    Fragment Callbacks

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -5,24 +5,29 @@ sap.ui.define([
 ], function (Controller, MessageToast) {
   "use strict";
   return Controller.extend("sap.ui.demo.ui5.controller.HelloPanel", {
-     onShowHello : function () {
-        // read msg from i18n model
-        var oBundle = this.getView().getModel("i18n").getResourceBundle();
-        var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-        // show message
-        MessageToast.show(sMsg);
-     },
-     onOpenDialog : function () {
-			// create dialog lazily
-			if (!this.pDialog) {
-				this.pDialog = this.loadFragment({
-					name: "sap.ui.demo.ui5.view.HelloDialog"
-				});
-			} 
-			this.pDialog.then(function(oDialog) {
-				oDialog.open();
-			});
+    onShowHello: function () {
+      // read msg from i18n model
+      var oBundle = this.getView().getModel("i18n").getResourceBundle();
+      var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+      var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+      // show message
+      MessageToast.show(sMsg);
+    },
+    onOpenDialog: function () {
+      // create dialog lazily
+      if (!this.pDialog) {
+        this.pDialog = this.loadFragment({
+          name: "sap.ui.demo.ui5.view.HelloDialog"
+        });
+      }
+      this.pDialog.then(function (oDialog) {
+        oDialog.open();
+      });
+    },
+    onCloseDialog : function () {
+			// note: We don't need to chain to the pDialog promise, since this event-handler
+			// is only called from within the loaded dialog itself.
+			this.byId("helloDialog").close();
 		}
   });
 });
