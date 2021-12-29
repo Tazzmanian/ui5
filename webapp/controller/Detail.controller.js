c60059aa4655385638ef9370a208985c4fa79c16
Tazzmanian <tazzmanian@abv.bg>
2021-12-29 22:24:08 +0200

    Custom Controls

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,14 +1,16 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/core/routing/History"
-], function (Controller, History) {
+	"sap/ui/core/routing/History",
+	"sap/m/MessageToast"
+], function (Controller, History, MessageToast) {
 	"use strict";
-	return Controller.extend("sap.ui.demo.ui5.controller.Detail", {
+	return Controller.extend("sap.ui.demo.iu5.controller.Detail", {
 		onInit: function () {
 			var oRouter = this.getOwnerComponent().getRouter();
 			oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
 		},
 		_onObjectMatched: function (oEvent) {
+			this.byId("rating").reset();
 			this.getView().bindElement({
 				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
 				model: "invoice"
@@ -25,6 +27,13 @@ sap.ui.define([
 				var oRouter = this.getOwnerComponent().getRouter();
 				oRouter.navTo("overview", {}, true);
 			}
+		},
+
+		onRatingChange: function (oEvent) {
+			var fValue = oEvent.getParameter("value");
+			var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+			MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
 		}
 	});
 });
